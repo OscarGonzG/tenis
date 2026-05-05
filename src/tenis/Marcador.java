dc283f520c4e735cb6368d7965f45b9a8c69b0cb
Óscar González <ogg453@alumnos.unican.es>
2026-05-05 02:12:53 +0200

    Registra datos de envío de puntuación en .csv

--- src/tenis/Marcador.java
+++ src/tenis/Marcador.java
@@ -2,9 +2,13 @@ package tenis;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import j2d.Juego;
 import j2d.mods.JObjetoVisNumTexto;
+import j2d.mods.multijugador.GestorMultijugadorCliente;
 import j2d.mods.multijugador.VariableRed;
 
 /**
@@ -14,7 +18,9 @@ import j2d.mods.multijugador.VariableRed;
  * @version jun-2026
  */
 public class Marcador extends JObjetoVisNumTexto {
-	
+
+	private File ficheroLog;
+	private FileWriter writer;
 	private static final int TAMANHO_FUENTE = 32;
 
 	private VariableRed<Integer> puntuacion;
@@ -25,13 +31,39 @@ public class Marcador extends JObjetoVisNumTexto {
 		asignaFuente(Font.DIALOG, Font.BOLD, TAMANHO_FUENTE);
 		asignaColor(Color.WHITE);
 		
-		puntuacion.anhadeSuscriptor(v -> asignaValor(v.valor()));
+		String nombreLog;
+		if (Juego.esCliente()) {
+			nombreLog = "cliente" + ((GestorMultijugadorCliente)Juego.gestorMultijugador()).idJugador();
+		} else {
+			nombreLog = "servidor";
+		}
+		ficheroLog = new File(nombre + "-" + nombreLog + ".csv");
+		try {
+			writer = new FileWriter(ficheroLog);
+			writer.write("valor,tiempoMs\n");
+		} catch (IOException e) {
+			System.out.println(-1);
+		}
+		puntuacion.anhadeSuscriptor(v -> {
+			try {
+				writer.write(v.valor() + "," + System.currentTimeMillis() + "\n");
+			} catch (IOException e) {
+				System.err.println("Recepcion de " + v.valor() + " no registrada");
+			}
+			asignaValor(v.valor());
+		});
 	}
 	
 	/**
 	 * Suma un punto.
 	 */
 	public void puntua() {
-		puntuacion.asignaValor(puntuacion.valor() + 1);
+		int nuevoValor = puntuacion.valor() + 1;
+		try {
+			writer.write(nuevoValor + "," + System.currentTimeMillis() + "\n");
+		} catch (IOException e) {
+			System.err.println("Envio de " + nuevoValor + " no registrado");
+		}
+		puntuacion.asignaValor(nuevoValor);
 	}
 }
