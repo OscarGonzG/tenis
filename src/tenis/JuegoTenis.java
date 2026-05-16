fd7576a9824532bd805bd55b4b398a61eda2a81f
Óscar González <ogg453@alumnos.unican.es>
2026-05-16 21:29:47 +0200

    feat: usa IP y puerto indicados para el servidor

--- src/tenis/JuegoTenis.java
+++ src/tenis/JuegoTenis.java
@@ -3,6 +3,10 @@ package tenis;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.net.UnknownHostException;
+
+import javax.swing.JOptionPane;
 
 import j2d.Juego;
 import j2d.mods.multijugador.GestorMultijugadorCliente;
@@ -15,10 +19,44 @@ import j2d.mods.multijugador.GestorMultijugadorCliente;
  */
 public class JuegoTenis {
 	public static void main(String[] args) {
+		
+		SocketAddress dirServidor = null;
+		while (dirServidor == null) {
+			String stringDirServidor = JOptionPane.showInputDialog(null,
+					"Introduce HOST:PUERTO del servidor",
+					"Indica el servidor", JOptionPane.PLAIN_MESSAGE);
+			
+			if (stringDirServidor == null) {
+				System.out.println("a");
+				System.exit(-1);
+			}
+			
+			String[] strings = stringDirServidor.split(":");
+			
+			if (strings.length != 2) {
+				System.out.println("b");
+				System.exit(-1);				
+			}
+			
+			try {
+				int puerto = Integer.parseInt(strings[1]);
+				InetAddress ipServidor = InetAddress.getByName(strings[0]);
+				dirServidor = new InetSocketAddress(ipServidor, puerto);
+			} catch (UnknownHostException e) {
+				JOptionPane.showMessageDialog(null, "Host \"" + strings[0] + 
+						"\" desconocido", "ERROR", JOptionPane.ERROR_MESSAGE);
+			} catch (IllegalArgumentException e ) {
+				JOptionPane.showMessageDialog(null, "Puerto \"" + strings[1] +
+						"\" no válido", "ERROR", JOptionPane.ERROR_MESSAGE);
+			}
+		}
+		
+		System.out.println(dirServidor);
+		
 		Juego.asignaNombre("Tenis");
 		Juego.asignaCiclosPorSegundo(120);
 		try {
-			Juego.asignaGestorMultijugador(new GestorMultijugadorCliente(null, new InetSocketAddress(InetAddress.getLocalHost(), 6000)));
+			Juego.asignaGestorMultijugador(new GestorMultijugadorCliente(null, dirServidor));
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.exit(-1);
--- src/tenis/ServidorTenis.java
+++ src/tenis/ServidorTenis.java
@@ -1,7 +1,12 @@
 package tenis;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.net.UnknownHostException;
+
+import javax.swing.JOptionPane;
 
 import j2d.Juego;
 import j2d.mods.multijugador.GestorMultijugadorServidor;
@@ -9,11 +14,43 @@ import j2d.mods.multijugador.GestorMultijugadorServidor;
 public class ServidorTenis {
 
 	public static void main(String[] args) {
+
+		SocketAddress dirServidor = null;
+		while (dirServidor == null) {
+			String stringDirServidor = JOptionPane.showInputDialog(null,
+					"Introduce HOST:PUERTO del servidor",
+					"Indica el servidor", JOptionPane.PLAIN_MESSAGE);
+			
+			if (stringDirServidor == null) {
+				System.out.println("a");
+				System.exit(-1);
+			}
+			
+			String[] strings = stringDirServidor.split(":");
+			
+			if (strings.length != 2) {
+				System.out.println("b");
+				System.exit(-1);				
+			}
+			
+			try {
+				int puerto = Integer.parseInt(strings[1]);
+				InetAddress ipServidor = InetAddress.getByName(strings[0]);
+				dirServidor = new InetSocketAddress(ipServidor, puerto);
+			} catch (UnknownHostException e) {
+				JOptionPane.showMessageDialog(null, "Host \"" + strings[0] + 
+						"\" desconocido", "ERROR", JOptionPane.ERROR_MESSAGE);
+			} catch (IllegalArgumentException e ) {
+				JOptionPane.showMessageDialog(null, "Puerto \"" + strings[1] +
+						"\" no válido", "ERROR", JOptionPane.ERROR_MESSAGE);
+			}
+		}
+		
 		GestorMultijugadorServidor servidor = null;
 		Juego.asignaCiclosPorSegundo(120);
 		try {
 		servidor =
-				new GestorMultijugadorServidor(2, new InetSocketAddress(6000));
+				new GestorMultijugadorServidor(2, dirServidor);
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.exit(-1);
